3715e4155cd31503e1f8b9926c94f4f3ccc341b3
RamDev <daudalaramadan2@gmail.com>
2025-12-10 12:56:40 +0000

    Refactor array.ts; update object examples and enhance function parameter types

--- array.ts
+++ array.ts
@@ -1,8 +1,14 @@
+
+
+
+
+
+
 // const arrals:  string[] = []
 
 // arrals.push("name")
 // console.log(arrals)
-
+/*
 const ourReadOnly:  [number, string,boolean ] = [1 ,"ei",true]
 
 ourReadOnly.push("5355")
@@ -32,15 +38,80 @@ const last: {name: string,age: number} = {
 
 console.log(last.age)   
 
+6
+//ojects with option
 
-///bojects with option 
-
-const list: {student: string,class?: number,seat: number} = {
+const list:  {readonly student: string,class?: number,seat: number} = {
     student: "shaban",
     seat: 5
 
 }
 
 console.log(list.seat)
+
+
+
+
+///objects with methods
+
+const letscalc =  {
+    add(hi: number,gii: number){
+        return hi + gii
+    }
+}
+console.log(letscalc.add(2,4))
+
+
+
+
+interface animal {
+    name: string
+    breed: string
+    year: number
+    data?: false;
+}
+
+const fanimal:  animal= {
+    name: "fjjj",
+    breed: "kk",
+    year: 84
+}
+
+
+console.log(fanimal.breed)
+
+
+
+///interface 
+interface Person {
+    name: string
+    age: number
+}
+
+
+///our object that we created
+const fperson: Person = {
+    name: "kofi",
+    age: 44
+}
+console.log(fperson.age)
+*/
+
+//✅ 2. Function parameter types
+interface Poet {
+    born: string;
+    name: string;
+}
+
+function usePoet(poet: Poet){
+    const born: string = poet.born;
+    console.log(`my name is ${poet.name} and i am ${born} years old`)
+}
+
+
+usePoet({
+    born: "44",
+    name: "jhdkj"
+})
 
 
