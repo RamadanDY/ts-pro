de90a270fa17014a7cd9b135918d0c7a531b3432
RamDev <daudalaramadan2@gmail.com>
2025-12-14 03:40:39 +0000

    Refactor School class and implement invoice class in class.js and class.ts

--- class.js
+++ class.js
@@ -1,10 +1,21 @@
-var School = /** @class */ (function () {
-    function School() {
+// const Ramadans = new School()
+// Ramadans.location = "hhhh"
+// Ramadans.name = "haha"
+// Ramadans.population = 444
+// console.log(Ramadans)
+////make a rep am for the last ai
+var invoice = /** @class */ (function () {
+    ///constructor
+    function invoice(b, a, i) {
+        this.brand = b;
+        this.amount = a;
+        this.isfact = i;
     }
-    return School;
+    // method
+    invoice.prototype.format = function () {
+        return "".concat(this.brand, " owes ").concat(this.amount, " for ").concat(this.isfact);
+    };
+    return invoice;
 }());
-var Ramadans = new School();
-Ramadans.location = "hhhh";
-Ramadans.name = "haha";
-Ramadans.population = 444;
-console.log(Ramadans);
+var inv1 = new invoice("rama", 300, "work on ai");
+console.log(inv1);
--- class.ts
+++ class.ts
@@ -1,14 +1,28 @@
-class School {
-    name: string;
-    location: string;
-    population: number
-    constructor (name: string,location: string,population: number){
+// const Ramadans = new School()
+// Ramadans.location = "hhhh"
+// Ramadans.name = "haha"
+// Ramadans.population = 444
+// console.log(Ramadans)
 
-    }
+////make a rep am for the last ai
+
+class invoice {
+  brand: string;
+  amount: number;
+  isfact: string;
+
+  ///constructor
+  constructor(b: string, a: number, i: string) {
+    this.brand = b;
+    this.amount = a;
+    this.isfact = i;
+  }
+  // method
+  format() {
+    return `${this.brand} owes ${this.amount} for ${this.isfact}`;
+  }
 }
 
-const Ramadans = new School()
-Ramadans.location = "hhhh"
-Ramadans.name = "haha"
-Ramadans.population = 444
-console.log(Ramadans)
+const inv1 = new invoice("rama", 300, "work on ai");
+
+console.log(inv1);
